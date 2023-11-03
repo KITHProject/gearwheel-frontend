8433091b1291f17d6780d557c97acf420ffff0d8
rynkovski <118986014+rynkovski@users.noreply.github.com>
2023-11-03 13:30:17 +0100

    feat: add new branch

---
+++ src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import {
+  loginUser,
+  logoutUser,
+  useAuthorizationStore,
+} from "@/stores/useAuthorizationStore";
+
+function LoginPanel() {
+  const username = useAuthorizationStore((state) => state.username);
+
+  return (
+    <div className="flex-col md:flex">
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4"></div>
+      </div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">{username}</h2>
+        </div>
+        <Button
+          onClick={() => {
+            logoutUser();
+          }}
+        >
+          Log out
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPanel;
--- src/stores/useAuthorizationStore.tsx
+++ src/stores/useAuthorizationStore.tsx
@@ -1,44 +1,38 @@
-import create from 'zustand'
-import { immer } from 'zustand/middleware/immer'
+import { AuthorizationStoreState } from "@/types/authorization.types";
+import create from "zustand";
+import { immer } from "zustand/middleware/immer";
 
-type AuthorizationStoreState = {
-  authorized: boolean
-  username: string
-  loginUser: (userName: string, password: string) => void
-  logoutUser: () => void
-  setUsername: (name: string) => void
-  setAuthorized: (flag: boolean) => void
-}
 export const useAuthorizationStore = create<AuthorizationStoreState>()(
   immer((set) => ({
-    username: '',
+    username: "",
     authorized: false,
     loginUser: async (username, password) => {
       set((state) => {
-        if(username !== 'admin' || password !== 'haslo123') {
-          return
+        if (username !== "admin" || password !== "haslo123") {
+          return;
         }
-        state.authorized = true
-        state.username = username
-      })
+        state.authorized = true;
+        state.username = username;
+      });
     },
     logoutUser: () => {
       set((state) => {
-        state.authorized = false
-      })
+        state.authorized = false;
+        state.username = "";
+      });
     },
     setUsername: (name) => {
       set((state) => {
-        state.username = name
-      })
+        state.username = name;
+      });
     },
     setAuthorized: (flag) => {
       set((state) => {
-        state.authorized = flag
-      })
+        state.authorized = flag;
+      });
     },
   }))
-)
+);
 
 export const { loginUser, logoutUser, setUsername, setAuthorized } =
-  useAuthorizationStore.getState()
+  useAuthorizationStore.getState();
