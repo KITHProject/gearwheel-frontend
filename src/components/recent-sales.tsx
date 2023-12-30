10413e69cdaa243a5fa2539d0f795af74300f23e
rynkovski <118986014+rynkovski@users.noreply.github.com>
2023-12-30 13:52:03 +0100

    design: dashboard changes

--- src/components/recent-sales.tsx
+++ src/components/recent-sales.tsx
@@ -1,67 +1,73 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+const mockSales = [
+  {
+    name: "Olivia Martin",
+    email: "olivia.martin@email.com",
+    sales: "+$1,999.00",
+    avatar: "OM",
+  },
+  {
+    name: "Krzysiem Wicki",
+    email: "kicky@email.com",
+    sales: "+$696969.00",
+    avatar: "KW",
+  },
+  {
+    name: "Karol Rynkowski",
+    email: "karolrynkowski@email.com",
+    sales: "+$1337.00",
+    avatar: "KR",
+  },
+  {
+    name: "Szymon Kowalski",
+    email: "szkmk@email.com",
+    sales: "+$7.00",
+    avatar: "SK",
+  },
+  {
+    name: "Daniel Golebiewski",
+    email: "danielgolebiewski@email.com",
+    sales: "+$420.00",
+    avatar: "DG",
+  },
+  {
+    name: "Krzysiem Wicki2",
+    email: "krzysiem@email.com",
+    sales: "+$696969.00",
+    avatar: "KW",
+  },
+  {
+    name: "Krzysiem Wicki3",
+    email: "krzysiem@email.com",
+    sales: "+$696969.00",
+    avatar: "KW",
+  },
+  {
+    name: "Krzysiem Wicki4",
+    email: "krzysiem@email.com",
+    sales: "+$696969.00",
+    avatar: "KW",
+  },
+];
+
 export function RecentSales() {
   return (
-    <div className="space-y-8">
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/01.png" alt="Avatar" />
-          <AvatarFallback>OM</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Olivia Martin</p>
-          <p className="text-sm text-muted-foreground">
-            olivia.martin@email.com
-          </p>
-        </div>
-        <div className="ml-auto font-medium">+$1,999.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="flex items-center justify-center space-y-0 border h-9 w-9">
-          <AvatarImage src="/avatars/02.png" alt="Avatar" />
-          <AvatarFallback>JL</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Jackson Lee</p>
-          <p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$39.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/03.png" alt="Avatar" />
-          <AvatarFallback>IN</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Isabella Nguyen</p>
-          <p className="text-sm text-muted-foreground">
-            isabella.nguyen@email.com
-          </p>
-        </div>
-        <div className="ml-auto font-medium">+$299.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/04.png" alt="Avatar" />
-          <AvatarFallback>WK</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">William Kim</p>
-          <p className="text-sm text-muted-foreground">will@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$99.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/05.png" alt="Avatar" />
-          <AvatarFallback>SD</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Sofia Davis</p>
-          <p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$39.00</div>
-      </div>
+    <div className="space-y-8 overflow-auto h-80">
+      {mockSales.map((person) => {
+        return (
+          <div key={person.name} className="flex items-center">
+            <Avatar className="h-9 w-9">
+              <AvatarFallback>{person.avatar}</AvatarFallback>
+            </Avatar>
+            <div className="ml-4 space-y-1">
+              <p className="text-sm font-medium leading-none">{person.name}</p>
+              <p className="text-sm text-muted-foreground">{person.email}</p>
+            </div>
+            <div className="ml-auto font-medium">{person.sales}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
