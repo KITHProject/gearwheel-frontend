78d045f643de143ac12377685ccf7d1ebd6001fd
rynkovski <118986014+rynkovski@users.noreply.github.com>
2023-12-30 13:51:32 +0100

    feat: forgot password card

--- src/components/forgot-password-card.tsx
+++ src/components/forgot-password-card.tsx
@@ -8,38 +8,49 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Separator } from "./ui/separator";
-import { LoginForm } from "./login-form";
+
 import { Cog } from "lucide-react";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 type Props = {
   setIsLogin: (isLogin: boolean) => void;
   setIsForgotPassword: (isForgotPassword: boolean) => void;
 };
-function LoginCard({ setIsLogin, setIsForgotPassword }: Props) {
+function ForgotPasswordCard({ setIsLogin, setIsForgotPassword }: Props) {
+  // const form = useForm<z.infer<typeof loginFormSchema>>({
+  //   resolver: zodResolver(loginFormSchema),
+  //   defaultValues: {
+  //     username: "",
+  //     password: "",
+  //   },
+  // });
   return (
     <Card className="w-full">
       <CardHeader className="relative">
-        <CardTitle className="text-center">Welcome Back!</CardTitle>
+        <CardTitle className="text-center">Forgot Password</CardTitle>
         <CardDescription className="flex items-center justify-center gap-1">
           <Cog size={20} />
           gearwheel
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <LoginForm />
-        <Button
-          onClick={() => setIsForgotPassword(false)}
-          className="p-0 mt-2"
-          variant={"link"}
-        >
-          Forgot your password?
-        </Button>
+        <CardDescription className="py-2 text-xs">
+          Enter the email address associated with your account and we'll send
+          you a link to reset your password.
+        </CardDescription>
+        <Label>Email</Label>
+        <Input placeholder="Enter your email" />
+        <Button className="w-full mt-2 rounded">Send</Button>
       </CardContent>
       <CardFooter className="flex flex-col gap-4">
         <Separator />
         <p>Don't have an account?</p>
         <Button
-          onClick={() => setIsLogin(false)}
+          onClick={() => {
+            setIsLogin(false);
+            setIsForgotPassword(false);
+          }}
           className="w-full rounded"
           variant={"outline"}
         >
@@ -50,4 +61,4 @@ function LoginCard({ setIsLogin, setIsForgotPassword }: Props) {
   );
 }
 
-export default LoginCard;
+export default ForgotPasswordCard;
