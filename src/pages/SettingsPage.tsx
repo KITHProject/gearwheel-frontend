d27efac53765e63c47e78d07bdf290882010a014
rynkovski <118986014+rynkovski@users.noreply.github.com>
2023-12-30 13:50:25 +0100

    chore: settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,67 +1,125 @@
-import Header from "@/components/header";
 import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Sidebar from "@/components/sidebar";
 
 function SettingsPage() {
   return (
-    <div className="flex-col flex min-h-screen relative">
-      <Header />
-      <div className="max-w-7xl sm:mx-auto my-4 px-2">
-        <div className="border rounded-xl shadow">
-          <div className="space-y-6 p-10">
+    <div className="relative flex bg-zinc-100">
+      <Sidebar />
+      <div className="px-2 my-4 max-w-7xl sm:mx-auto">
+        <div className="border shadow rounded-xl bg-primary-foreground">
+          <div className="p-10 space-y-6">
             <div className="border-b space-y-0.5 pb-6">
               <h2 className="text-xl font-bold">Settings</h2>
               <p className="text-muted-foreground">
                 Manage your account settings and set e-mail preferences.
               </p>
             </div>
-            <div className="flex flex-col lg:flex-row  items-start space-y-8 lg:space-x-12 lg:space-y-0">
-              <aside className="-mx-2 lg:w-1/5">
-                <nav className="flex items-center justify-center lg:flex-col ">
-                  <a className="w-full py-2 px-4 hover:bg-gray-400 hover:text-white rounded-md cursor-pointer">
-                    Account
-                  </a>
-                  <a className="w-full py-2 px-4 hover:bg-gray-400 hover:text-white rounded-md cursor-pointer">
-                    Display
-                  </a>
-                  <a className="w-full py-2 px-4 hover:bg-gray-400 hover:text-white rounded-md cursor-pointer">
-                    Help
-                  </a>
-                  <a className="w-full py-2 px-4 hover:bg-gray-400 hover:text-white rounded-md cursor-pointer">
-                    FAQ
-                  </a>
-                </nav>
-              </aside>
-              <div className="flex-1 lg:max-w-2xl">
-                <div className="space-y-6">
-                  <div>
-                    <h3 className="text-lg font-bold">Profile</h3>
-                    <p className="text-muted-foreground">
-                      This is how others will see you on the site.
-                    </p>
-                  </div>
-                  <form className="space-y-8">
-                    <div className="space-y-2 flex flex-col">
-                      <label htmlFor="username">Username</label>
-                      <Input required placeholder="Your Username" />
-                      <p className="text-muted-foreground text-sm">
+
+            <Tabs defaultValue="account" className="w-[500px]">
+              <TabsList className="grid w-full grid-cols-4">
+                <TabsTrigger value="account">Account</TabsTrigger>
+                <TabsTrigger value="password">Password</TabsTrigger>
+                <TabsTrigger value="help">Help</TabsTrigger>
+                <TabsTrigger value="faq">FAQ</TabsTrigger>
+              </TabsList>
+              <TabsContent value="account">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Account</CardTitle>
+                    <CardDescription>
+                      Make changes to your account here. Click save when you're
+                      done.
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent className="space-y-2">
+                    <div className="space-y-1">
+                      <Label htmlFor="name">Name</Label>
+                      <Input id="name" defaultValue="Pedro Duarte" />
+                    </div>
+                    <div className="space-y-1">
+                      <Label htmlFor="username">Username</Label>
+                      <Input id="username" defaultValue="@peduarte" />
+                      <CardDescription>
                         This is your public display name. It can be your real
                         name or a pseudonym. You can only change this once every
                         30 days.
-                      </p>
+                      </CardDescription>
                     </div>
-                    <div className="space-y-2 flex flex-col">
-                      <label htmlFor="username">Username</label>
-                      <Input required placeholder="Your Username" />
-                      <p className="text-muted-foreground text-sm">
-                        This is your public display name. It can be your real
-                        name or a pseudonym. You can only change this once every
-                        30 days.
-                      </p>
+                  </CardContent>
+                  <CardFooter>
+                    <Button>Save changes</Button>
+                  </CardFooter>
+                </Card>
+              </TabsContent>
+              <TabsContent value="password">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Password</CardTitle>
+                    <CardDescription>
+                      Change your password here. After saving, you'll be logged
+                      out.
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent className="space-y-2">
+                    <div className="space-y-1">
+                      <Label htmlFor="current">Current password</Label>
+                      <Input id="current" type="password" />
                     </div>
-                  </form>
-                </div>
-              </div>
-            </div>
+                    <div className="space-y-1">
+                      <Label htmlFor="new">New password</Label>
+                      <Input id="new" type="password" />
+                    </div>
+                  </CardContent>
+                  <CardFooter>
+                    <Button>Save password</Button>
+                  </CardFooter>
+                </Card>
+              </TabsContent>
+              <TabsContent value="help">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>HELP</CardTitle>
+                    <CardDescription>...</CardDescription>
+                  </CardHeader>
+                  <CardContent className="space-y-2">...</CardContent>
+                </Card>
+              </TabsContent>
+              <TabsContent value="faq">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>FAQ</CardTitle>
+                    <CardDescription>...</CardDescription>
+                  </CardHeader>
+                  <CardContent className="space-y-2">
+                    <Accordion type="single" collapsible>
+                      <AccordionItem value="item-1">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                          Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  </CardContent>
+                </Card>
+              </TabsContent>
+            </Tabs>
           </div>
         </div>
       </div>
